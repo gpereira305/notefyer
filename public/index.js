bafb70e8afe3452ee7d30fcd29c28b4e2611379b
Giovane Pereira <giovane.pereira@ixcsoft.com.br>
2026-07-22 17:28:22 -0300

    feat: add clear notifications, alert for minimum chars in text message

--- public/index.js
+++ public/index.js
@@ -2,11 +2,18 @@ const form = document.getElementById("notificationForm");
 const tableBody = document.getElementById("notificationsTable");
 const emailInput = document.getElementById("email");
 const messageInput = document.getElementById("message");
+const clearButton = document.getElementById("clearButton");
+const sendButton = document.getElementById("sendButton");
 
-loadNotifications().catch(error => console.error("Error loading notifications:", error));
+loadNotifications().catch(error => console.error("Erro ao carregar notificações:", error));
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
+
+    if (messageInput.value.length <= 5) {
+        alert("A mensagem deve ter mais de 5 caracteres!");
+        return;
+    }
 
     try {
         const response = await fetch("api.php", {
@@ -24,11 +31,11 @@ form.addEventListener("submit", async (e) => {
             form.reset();
             await loadNotifications();
         } else {
-            showError(result.error || "Falha ao enviar notificação.");
+            showError(result.error || "Falha ao enviar notificação");
         }
     } catch (error) {
         console.error("Error:", error);
-        showError("Erro de rede ao contatar o servidor.");
+        showError("Erro de rede ao contatar o servidor");
     }
 });
 
@@ -37,6 +44,7 @@ async function loadNotifications() {
         const response = await fetch("api.php");
         const data = await response.json();
         handleMountNotificationTable(data);
+        displayClearButton(data);
     } catch (error) {
         console.error("Error:", error);
     }
@@ -74,9 +82,39 @@ function showEmptyMessage() {
     tableBody.innerHTML = `<tr><td class="empty" colspan="4">Não há mensagens :(</td></tr>`;
 }
 
-function escapeHtml(s) {
-    return String(s ?? "").replace(/[&<>"']/g, (c) => ({
+function escapeHtml(value) {
+    return String(value ?? "").replace(/[&<>"']/g, (replaceBy) => ({
         "&": "&amp;", "<": "&lt;", ">": "&gt;",
         '"': "&quot;", "'": "&#39;"
-    }[c]));
+    }[replaceBy]));
+}
+
+clearButton.addEventListener("click", async () => {
+    if (!confirm("Tem certeza que deseja limpar todas as notificações?")) {
+        return;
+    }
+
+    try {
+        const response = await fetch("api.php", {
+            method: "DELETE",
+        });
+        const result = await response.json();
+
+        if (result.success) {
+            await loadNotifications();
+        } else {
+            showError(result.error || "Falha ao limpar notificações!");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        showError("Erro de rede ao contatar o servidor!");
+    }
+});
+
+function displayClearButton(notifications) {
+    if (notifications.length > 0) {
+        clearButton.style.display = "block";
+    } else {
+        clearButton.style.display = "none";
+    }
 }
